fb7bbf61956db5de0f0dc32e1e2272e25c34f5a4
KiokiKaller <77280407+KiokiKaller@users.noreply.github.com>
2021-01-11 14:28:41 +0100

    Add files via upload

---
+++ MineSweeper_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef MINESWEEPER_PRIVATE_H
+#define MINESWEEPER_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"0.1.1.1"
+#define VER_MAJOR	0
+#define VER_MINOR	1
+#define VER_RELEASE	1
+#define VER_BUILD	1
+#define COMPANY_NAME	"(C)Haha Inc"
+#define FILE_VERSION	"1.0"
+#define FILE_DESCRIPTION	"Mine Sweeper"
+#define INTERNAL_NAME	"winminer"
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"MineSweeper"
+#define PRODUCT_NAME	"Mine Sweeper"
+#define PRODUCT_VERSION	"1.0"
+
+#endif /*MINESWEEPER_PRIVATE_H*/
